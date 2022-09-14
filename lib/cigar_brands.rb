06cb0d2693ebc3b0d1fd0c1e5e6d9f5590cc394e
Janan Akkad <jananakkad@hotmail.co.uk>
2022-09-14 16:46:57 +0100

    added a CLI class

--- lib/cigar_brands.rb
+++ lib/cigar_brands.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "cigar_brands/version"
+require_relative "cigar_brands/cli.rb"
 
 module CigarBrands
   class Error < StandardError; end
   # Your code goes here...
 end
+
+# may need to rename this file, environment
---
+++ lib/cigar_brands/cli.rb
@@ -0,0 +1,9 @@
+class CigarBrands::CLI 
+    def call
+        puts ""
+        puts "Welcome to Cigar Brands!"
+        puts ""
+        puts "Here you can find information on different cigar brands!"
+        puts ""
+    end
+end
