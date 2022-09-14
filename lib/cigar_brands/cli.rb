96ce93bcf5c7fecb42c5a7aa44c42509a6ade03b
Janan Akkad <jananakkad@hotmail.co.uk>
2022-09-14 19:11:47 +0100

    added additional methods to cli class

--- lib/cigar_brands/cli.rb
+++ lib/cigar_brands/cli.rb
@@ -10,7 +10,7 @@ class CigarBrands::CLI
         puts "Here you can find information on different cigar brands!"
         puts ""
         get_page_numbers
-        list_page_results
+        list_page_numbers
         get_user_page_number
         # puts "\n#{@@grn}There are 51 brands available, to view them, please type "brands" or type "exit" to exit this programme#{@@white}\n" # list_brands
         # user_chooses_brand(number)
@@ -27,7 +27,7 @@ class CigarBrands::CLI
         @page_numbers = [7, 8, 9]
     end
 
-    def list_page_results
+    def list_page_numbers
         # list results    
         puts "\n#{@@grn}Choose a page number to see a list of brands.#{@@white}\n"
         puts ""
@@ -38,12 +38,16 @@ class CigarBrands::CLI
 
     def get_user_page_number
         chosen_page = gets.strip.to_i
-        binding.pry
-        # if valid_input(chosen_page, @page_numbers)
-        # end
+        show_results_for(chosen_page) if valid_input(chosen_page, @page_numbers)
     end
 
     def valid_input(input, data)
         input.to_i <= @page_numbers.length && input.to_i > 0
+    end
+
+    def show_results_for(chosen_page)
+        page = @page_numbers[chosen_page + 1] #or should it be - 1?
+        puts "Here are the results for #{page}"
+        binding.pry
     end
 end
