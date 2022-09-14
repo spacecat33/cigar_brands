4e8c35d4411f6a92c1f2da86c8768873c06657ae
Janan Akkad <jananakkad@hotmail.co.uk>
2022-09-14 15:27:52 +0100

    created gem cigar_brands

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in cigar_brands.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/cigar_brands.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cigar_brands/version"
+
+module CigarBrands
+  class Error < StandardError; end
+  # Your code goes here...
+end
